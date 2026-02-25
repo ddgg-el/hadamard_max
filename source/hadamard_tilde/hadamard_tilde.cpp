cf5440190d209b02e236a8b82a61194bbb013910
Davide Gagliardi <info@davidegagliardi.eu>
2026-02-26 00:33:35 +0100

    added order arguments and better class initialization

--- source/hadamard_tilde/hadamard_tilde.cpp
+++ source/hadamard_tilde/hadamard_tilde.cpp
@@ -1,37 +1,36 @@
 /// @file
-///	@ingroup 	ddgg
-///	@copyright	Copyright 2018 The Min-DevKit Authors. All rights reserved.
+///	@ingroup 	processing
+///	@copyright	Copyright 2026 The Authors. All rights reserved.
 ///	@license	Use of this source code is governed by the MIT License found in the License.md file.
 
 #include "c74_min.h"
 #include "c74_min_api.h"
-#include <cstddef>
-// #include "../shared/signal_routing_objects.h"
-// #include "c74_min_atom.h"
-// #include <memory>
 
 
 class hadamard : public c74::min::object<hadamard>, public c74::min::vector_operator<> {
-private: 
-	std::vector<std::unique_ptr<c74::min::inlet<>>> m_inlets;
-	std::vector<std::unique_ptr<c74::min::outlet<>>> m_outlets;
 public:
-	MIN_DESCRIPTION {"Walsh-Hadamard Transform"};
+	MIN_DESCRIPTION {"Walsh-Hadamard Transform Object"};
 	MIN_TAGS {"audio, processing"};
-	MIN_AUTHOR {"Davide Gagliardi, Davide Bardi"};
+	MIN_AUTHOR {"Davide Gagliardi - Davide Bardi"};
 
-	int m_channels = 0;
+	c74::min::argument<c74::min::number> order_arg { this, " matrix order", "Order of the matrix", true};
 
 	hadamard(const c74::min::atoms& args = {}) {
-		if(args.size() > 0) {
-			m_channels = args[0];	
-		} else {
-			m_channels = 4;
+		c74::min::error(args.size() < 1 && !c74::min::dummy(), "hadamard~: Missing argument `order`");
+	
+		m_order = static_cast<int>(args[0]);	
+		if(m_order > 7) {
+			cwarn << "Order too high! clamping at 7" << c74::min::endl;
+			m_order = 7;
+		} else if(m_order < 0) {
+			cwarn << "Order cannot be negative! Setting it to 0" << c74::min::endl;
+			m_order = 1;
 		}
-		auto inlet_count = m_channels;
-		auto outlet_count = m_channels;
+	
 
-		for (auto i = 0; i < int(inlet_count); ++i) {
+		int n_channels = static_cast<int>(pow(2, m_order));
+
+		for (auto i = 0; i < n_channels; ++i) {
 			    auto an_inlet = std::make_unique<c74::min::inlet<>>(this, "(signal) an input channel", "signal");
 				auto an_outlet = std::make_unique<c74::min::outlet<>>(this, "(signal) an outputchannel", "signal");
 				m_inlets.push_back(std::move(an_inlet));
@@ -43,8 +42,8 @@ public:
 
 	c74::min::message<> dspsetup { this, "dspsetup", 
     MIN_FUNCTION {
-			c74::min::number samplerate = args[0];
-			int vectorsize = args[1];
+			this->samplerate(args[0]);
+			this->vector_size(args[1]);
 			return {};
 		}
 	};	
@@ -59,6 +58,10 @@ public:
 			}
 		}
 	}
+private: 
+	std::vector<std::unique_ptr<c74::min::inlet<>>> m_inlets;
+	std::vector<std::unique_ptr<c74::min::outlet<>>> m_outlets;
+	int m_order = 0;
 };
 
 MIN_EXTERNAL(hadamard);
