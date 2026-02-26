db079dd67c64ba57172917ad6db8bcd2f5ffd7ca
Davide Gagliardi <info@davidegagliardi.eu>
2026-02-26 01:32:18 +0100

    working prototype

--- source/hadamard_tilde/hadamard_tilde.cpp
+++ source/hadamard_tilde/hadamard_tilde.cpp
@@ -5,6 +5,9 @@
 
 #include "c74_min.h"
 #include "c74_min_api.h"
+#include "c74_min_logger.h"
+#include <cstddef>
+#include <vector>
 
 
 class hadamard : public c74::min::object<hadamard>, public c74::min::vector_operator<> {
@@ -16,52 +19,83 @@ public:
 	c74::min::argument<c74::min::number> order_arg { this, " matrix order", "Order of the matrix", true};
 
 	hadamard(const c74::min::atoms& args = {}) {
-		c74::min::error(args.size() < 1 && !c74::min::dummy(), "hadamard~: Missing argument `order`");
-	
-		m_order = static_cast<int>(args[0]);	
+		
+		if(args.size() > 0) {
+			m_order = static_cast<int>(args[0]);	
+		} else {
+			cwarn << "Argument `order` not specified. Keeping it default to " << m_order << c74::min::endl; 
+		}
 		if(m_order > 7) {
 			cwarn << "Order too high! clamping at 7" << c74::min::endl;
 			m_order = 7;
-		} else if(m_order < 0) {
-			cwarn << "Order cannot be negative! Setting it to 0" << c74::min::endl;
+		} else if(m_order < 1) {
+			cwarn << "Order lower than 1! Setting it back to default ( " << m_order << " )" << c74::min::endl;
 			m_order = 1;
 		}
 	
 
-		int n_channels = static_cast<int>(pow(2, m_order));
+		m_channels = static_cast<int>(pow(2, m_order));
 
-		for (auto i = 0; i < n_channels; ++i) {
+		for (auto i = 0; i < m_channels; ++i) {
 			    auto an_inlet = std::make_unique<c74::min::inlet<>>(this, "(signal) an input channel", "signal");
 				auto an_outlet = std::make_unique<c74::min::outlet<>>(this, "(signal) an outputchannel", "signal");
 				m_inlets.push_back(std::move(an_inlet));
 				m_outlets.push_back(std::move(an_outlet));
-		}	
+		}
+
+		m_frame.assign(m_channels, 0.0);
+		m_norm = 1.0 / std::sqrt(m_channels);
 	}
 
 	~hadamard() {}
 
 	c74::min::message<> dspsetup { this, "dspsetup", 
     MIN_FUNCTION {
-			this->samplerate(args[0]);
-			this->vector_size(args[1]);
+			samplerate(args[0]);
+			vector_size(args[1]);
 			return {};
 		}
 	};	
 
 
 	void operator()(c74::min::audio_bundle input, c74::min::audio_bundle output) {
-		for(auto ch = 0; ch < output.channel_count(); ++ch) {
-			auto in = input.samples(ch);
-			auto out = output.samples(ch);
-			for (auto sample = 0; sample < output.frame_count(); ++sample) {
-				out[sample] = in[sample];
+		for(auto sample = 0; sample < output.frame_count(); ++sample) {
+			for (auto ch = 0; ch < output.channel_count(); ++ch) {
+				m_frame[ch] = input.samples(ch)[sample];
+			}
+
+			fwht(m_frame.data(), output.channel_count());
+
+			for (auto ch = 0; ch < output.channel_count(); ++ch) {
+				output.samples(ch)[sample] = m_frame[ch] * m_norm;
 			}
 		}
 	}
 private: 
+
+	void fwht(double *a, size_t n) {
+		for (size_t h=1; h < n; h *= 2){
+			for (size_t i = 0; i < n; i += h*2) {
+				for (size_t j = i; j < i+h; j++) {
+					double x = a[j];
+					double y = a[j+h];
+					a[j] = x + y;
+					a[j+h] = x - y;
+				}
+			}
+		}
+		for (int i = 0; i < n; i++){
+			a[i] = a[i]*(1/pow(2,log2f(n)-1));
+		}
+	}
+
 	std::vector<std::unique_ptr<c74::min::inlet<>>> m_inlets;
 	std::vector<std::unique_ptr<c74::min::outlet<>>> m_outlets;
-	int m_order = 0;
+	int m_order { 1 };
+	int m_channels { 2 };
+	std::vector<double> m_frame;
+	double m_norm { 0.0 };
+
 };
 
 MIN_EXTERNAL(hadamard);
